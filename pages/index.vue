6148ce8b5d69ead1fed0afda45cc8d850945e88d
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-16 13:23:41 +0900

    Test ボタンコンポーネント表示テスト

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,20 @@
 <template>
-  
+  <div class="inner">
+    <Button label="はじめる" @click="isLogin" />
+  </div>
 </template>
 
 <script>
-export default {}
+import Button from '~/components/Button';
+export default {
+  components: {
+    Button,
+  },
+  methods: {
+    isLogin(){
+      $router.push('/login')
+    }
+  }
+}
 </script>
 
