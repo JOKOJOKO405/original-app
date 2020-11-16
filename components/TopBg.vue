9e8bce529c58f1096cd2cc4d84bb9cc5717fbffa
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-16 14:40:54 +0900

    Add トップ画面背景作成

---
+++ components/TopBg.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bg-img">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/css/main";
+  .bg-img{
+    position: absolute;
+    background: url('~@/assets/img/bg_img.png') center center repeat-y;
+    background-size: contain;
+    width: 100%;
+    height: 100%;
+    z-index: -2;
+    &::before{
+      @extend %psedo;
+      width: 100%;
+      height: 100%;
+      background: rgba(#000, 0.5);
+      top: 0;
+      left: 0;
+      z-index: -1;
+    }
+  }
+</style>
