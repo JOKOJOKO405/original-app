ecf5f5b4323c500c88f277e23977841b98b6c753
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-17 06:08:45 +0900

    Add ユーザーページ

---
+++ pages/user/_id.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="inner">
+    ここはユーザーページ
+    <ModalBase v-if="isModalOpened" @close-modal="closeModal">
+      <EditProfile />
+    </ModalBase>
+    一覧
+    <AddButton @add-card="addCard" />
+  </div>
+</template>
+
+<script>
+import ModalBase from '~/components/ModalBase'
+import EditProfile from '~/components/EditProfile'
+import AddButton from '~/components/AddButton'
+export default {
+  components: {
+    ModalBase,
+    EditProfile,
+    AddButton,
+  },
+  data(){
+    return {
+      isModalOpened: true,
+    }
+  },
+  methods: {
+    closeModal(){
+      this.isModalOpened = false
+    },
+    addCard(){
+      this.isModalOpened = true
+    },
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
