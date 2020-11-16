c325bd8d111229711ea60614bdbb6dd9aaa44fdf
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-16 13:23:20 +0900

    Add ボタンコンポーネント作成

---
+++ components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="btn-primary">
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['label'],
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/css/main";
+  .btn-primary{
+    background: $primary-color;
+    text-align: center;
+    font-weight: bold;
+    color: #fff;
+    padding: 10px;
+    border-radius: $radius-lr;
+    display: block;
+    width: 100%;
+}
+</style>
