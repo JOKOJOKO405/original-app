73dfa9e745863dd0c91746efc0ec6fbb423a399f
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-16 13:55:38 +0900

    Fix ボタンスタイル@includeで

--- components/Button.vue
+++ components/Button.vue
@@ -13,13 +13,7 @@ export default {
 <style lang="scss">
 @import "@/assets/css/main";
   .btn-primary{
-    background: $primary-color;
-    text-align: center;
-    font-weight: bold;
-    color: #fff;
-    padding: 10px;
-    border-radius: $radius-lr;
-    display: block;
-    width: 100%;
+    @include btn($primary-color, #fff);
+    margin-bottom: 16px;
 }
 </style>
