968be229f08f2d232fa13d2ffb8f46c90bc4f36a
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-16 15:34:36 +0900

    Add フォーム入力input

--- components/Form.vue
+++ components/Form.vue
@@ -1,23 +1,44 @@
 <template>
-  <div class="inner">
-    <v-form
+  <v-form
       ref="form"
       lazy-validation
     >
+    <span class="label">メールアドレス</span>
+    <v-text-field
+      label="メールアドレス"
+      single-line
+      outlined
+    ></v-text-field>
+    <span class="label">パスワード</span>
+    <v-text-field
+      label="半角英数字8文字以上"
+      single-line
+      outlined
+    ></v-text-field>
+    <template v-if="isSignUp">
       <v-text-field
-        label="Outlined"
+        label="もう一度入力してください"
+        single-line
         outlined
       ></v-text-field>
-    </v-form>
-  </div>
+    </template>
+  </v-form>
 </template>
 
 <script>
 export default {
-  
+  data(){
+    return {
+      isSignUp: false
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-
+  .label{
+    display: block;
+    margin-bottom: 8px;
+    font-weight: bold;
+  }
 </style>
