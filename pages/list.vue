70dee68301f484210a55bd080632c8bee2247f4d
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-16 16:33:53 +0900

    Test モーダル表示

--- pages/list.vue
+++ pages/list.vue
@@ -1,12 +1,35 @@
 <template>
   <main>
+    <ModalBase v-if="isModalOpened" @close-modal="closeModal">
+      ここに何かはいる
+    </ModalBase>
     一覧
+    <AddButton @add-card="addCard" />
+
   </main>
 </template>
 
 <script>
+import ModalBase from '~/components/ModalBase'
+import AddButton from '~/components/AddButton'
 export default {
-  
+  components: {
+    ModalBase,
+    AddButton,
+  },
+  data(){
+    return {
+      isModalOpened: true,
+    }
+  },
+  methods: {
+    closeModal(){
+      this.isModalOpened = false
+    },
+    addCard(){
+      this.isModalOpened = true
+    }
+  }
 }
 </script>
 
