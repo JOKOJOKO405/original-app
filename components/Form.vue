8dd00ed9af01689702bc9c7e0dca969cb1372e95
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-16 15:11:58 +0900

    Add formテンプレート追加

---
+++ components/Form.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="inner">
+    <v-form
+      ref="form"
+      lazy-validation
+    >
+      <v-text-field
+        label="Outlined"
+        outlined
+      ></v-text-field>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+
+</style>
