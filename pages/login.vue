24f492de0ad8c25dd4fa956b41a3b540cded44d6
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-16 15:46:16 +0900

    Add ログイン画面

--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="inner">
-    表示テスト/ログインページ
-  </div>
+  <main>
+    <div class="inner">
+      <Form />
+      <Button label="ログイン" @on-submit="onSubmit" />
+      <GoogleLogin label="Googleアカウントでログイン" @google-login="googleLogin" />
+    </div>
+  </main>
 </template>
 
 <script>
+import Form from '~/components/Form';
+import Button from '~/components/Button'
+import GoogleLogin from '~/components/GoogleLogin'
 export default {
+  components: {
+    Form,
+  },
+  methods: {
+    googleLogin(){
+      console.log('ok')
+    },
+    onSubmit(){
+      this.$router.push('/list')
+    }
+  }
   
 }
 </script>
