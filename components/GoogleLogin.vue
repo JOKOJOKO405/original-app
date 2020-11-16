ab953cd001392bc14b51ef8edc53a0fb70e678bc
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-16 13:55:59 +0900

    Add googleログインボタン作成

---
+++ components/GoogleLogin.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="btn-google" @click="$emit('google-login')">
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['label'],
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/css/main";
+  .btn-google{
+    @include btn(#fff, $font-color);
+    border: 1px solid $light-gray;
+    position: relative;
+    &::before{
+      @extend %psedo;
+      width: 24px;
+      height: 24px;
+      background: url('~@/assets/img/google-icon.svg') center center no-repeat;
+      background-size: cover;
+      top: 50%;
+      transform: translateY(-50%);
+      left: 16px;
+    }
+}
+</style>
