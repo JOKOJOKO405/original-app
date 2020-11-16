729e7800f6c95238d8a601593a623d0907b83596
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-16 16:33:02 +0900

    Add 新規投稿ボタンコンポーネント

---
+++ components/AddButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="addBtn" @click="$emit('add-card')">
+    <img src="@/assets/img/ico_plus.svg" alt="">
+  </button>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/css/main";
+  .addBtn {
+    @include circleBtn(60px, 16px);
+    bottom: 16px;
+  }
+</style>
