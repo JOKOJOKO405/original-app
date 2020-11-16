ac67a01a0a25b40a85e4613e179f0ee2fc686927
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-16 15:46:29 +0900

    Add 新規登録画面

---
+++ pages/signup.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <div class="inner">
+      <Form />
+      <Button label="はじめる" @on-submit="onSubmit" />
+      <GoogleLogin label="Googleアカウントではじめる" @google-login="googleLogin" />
+    </div>
+  </main>
+</template>
+
+<script>
+import Form from '~/components/Form';
+import Button from '~/components/Button'
+import GoogleLogin from '~/components/GoogleLogin'
+export default {
+  components: {
+    Form,
+  },
+  methods: {
+    googleLogin(){
+      console.log('ok')
+    },
+    onSubmit(){
+      this.$router.push('/list')
+    }
+  }
+  
+}
+</script>
