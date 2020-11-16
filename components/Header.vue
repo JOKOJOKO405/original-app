7f76bbcc31214df7719afc630730d1be7ac6e67e
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-16 11:07:20 +0900

    Test scssインポートテスト

---
+++ components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import '@/assets/css/main';
+  header {
+    background: $primary-gradient;
+    padding: 16px;
+  }
+</style>
