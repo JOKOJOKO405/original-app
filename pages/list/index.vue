bebcbdedad376a9d59b1e4ee18025d4044ef1d2b
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-18 15:49:57 +0900

    Add 一覧ページ中身

--- pages/list/index.vue
+++ pages/list/index.vue
@@ -1,15 +1,31 @@
 <template>
-  <main>
-    <ModalBase v-if="isModalOpened" @close-modal="closeModal">
-      <CreatePost />
-    </ModalBase>
-    一覧
-    <AddButton @add-card="addCard" />
-
-  </main>
+  <div class="wrapper">
+    <div class="inner">
+      <SortDialog />
+    </div>
+    <div class="bg-col inner">
+      <ul class="list__container">
+        <li class="list__item">
+          <DetailCard />
+        </li>
+        <li class="list__item">
+          <DetailCard />
+        </li>
+        <li class="list__item">
+          <DetailCard />
+        </li>
+      </ul>
+      <ModalBase v-if="isModalOpened" @close-modal="closeModal">
+        <CreatePost />
+      </ModalBase>
+      <AddButton @add-card="addCard" />
+    </div>
+  </div>
 </template>
 
 <script>
+import DetailCard from '~/components/DetailCard'
+import SortDialog from '~/components/SortDialog'
 import ModalBase from '~/components/ModalBase'
 import CreatePost from '~/components/CreatePost'
 import AddButton from '~/components/AddButton'
@@ -18,6 +34,8 @@ export default {
     ModalBase,
     AddButton,
     CreatePost,
+    DetailCard,
+    SortDialog,
   },
   data(){
     return {
@@ -36,5 +54,21 @@ export default {
 </script>
 
 <style lang="scss">
-
+  .bg-col{
+    background: $bg-color;
+  }
+  .list{
+    &__container{
+      display: flex;
+      width: 100%;
+      flex-wrap: wrap;
+    }
+    &__item{
+      &:not(:nth-child(2n)){
+        margin-right: 16px;
+      }
+      margin-bottom: 16px;
+      width: calc(50% - 8px);
+    }
+  }
 </style>
