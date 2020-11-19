db011ac11d200ecd8d4fa983a1ee11ce9ad280ab
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-19 13:02:26 +0900

    Add usersのstore作成

---
+++ store/user.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+export const state = () => ({
+  users: []
+})
+
+export const mutations = {
+
+}
+
+export const actions = {
+
+}
+
+export const getters = {
+  
+}
