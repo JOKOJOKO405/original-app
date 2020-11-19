62142de29a58d81f3e938c019ef68dbbb4f4bed4
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-19 15:10:35 +0900

    Add google認証ログインaction in store

--- store/user.js
+++ store/user.js
@@ -1,17 +1,18 @@
-import firebase from 'firebase'
+import firebase from '~/plugins/firebase'
 
 export const state = () => ({
   users: []
-})
+});
 
-export const mutations = {
-
-}
+export const mutations = {};
 
 export const actions = {
+  googleLogin() {
+    // Google プロバイダ オブジェクトのインスタンスを作成
+    const googleAuth_provider = new firebase.auth.GoogleAuthProvider();
+    // ログインページにリダイレクトしてログインを行う
+    firebase.auth().signInWithRedirect(googleAuth_provider);
+  }
+};
 
-}
-
-export const getters = {
-  
-}
+export const getters = {};
