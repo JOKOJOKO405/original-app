e45ef2610938061127a9b8e9f4feac80fa265c3f
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-21 16:38:07 +0900

    Add vuetify バリデーション

--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,31 @@
 <template>
   <div class="inner">
-      <Form />
+    <v-form ref="form" lazy-validation>
+      <span class="label">メールアドレス</span>
+      <v-text-field
+        v-model="form.email.val"
+        label="メールアドレス"
+        single-line
+        outlined
+        :rules="form.email.rules"
+      ></v-text-field>
+      <span class="label">パスワード</span>
+      <v-text-field
+        v-model="form.pass.val"
+        :append-icon="form.pass.isShow ? 'mdi-eye' : 'mdi-eye-off'"
+        :rules="form.pass.rules"
+        :type="form.pass.isShow ? 'text' : 'password'"
+        label="半角英数字8文字以上12文字以内"
+        single-line
+        outlined
+        @click:append="form.pass.isShow = !form.pass.isShow"
+      ></v-text-field>
       <Button label="ログイン" @event="onSubmit" />
-      <GoogleLogin label="Googleアカウントでログイン" @google-login="googleLogin" />
+    </v-form>
+    <GoogleLogin
+        label="Googleアカウントでログイン"
+        @google-login="googleLogin"
+      />
   </div>
 </template>
 
@@ -14,12 +37,40 @@ export default {
   components: {
     Form,
   },
+  data() {
+    return {
+      form: {
+        email: {
+          val: "",
+          rules: [
+            (val) => !!val || "メールアドレスを入力してください",
+            (val) => {
+              const regex = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/;
+              return regex.test(val) || "入力が正しくありません";
+            },
+          ],
+        },
+        pass: {
+          val: "",
+          isShow: false,
+          rules: [
+            (val) => !!val || "パスワードを入力してください",
+            (val) => {
+              const regex = /^[A-Za-z0-9]*$/;
+              return regex.test(val) || "半角英数字で入力してください";
+            },
+            (val) => (val.length >= 8 && val.length < 13) || "8文字以上12文字以内で入力してください",
+          ],
+        },
+      },
+    }
+  },
   methods: {
     googleLogin(){
-      console.log('ok')
+      this.$store.dispatch("user/googleLogin");
     },
     onSubmit(){
-      this.$router.push('/list')
+      this.$router.push('/user')
     }
   }
   
