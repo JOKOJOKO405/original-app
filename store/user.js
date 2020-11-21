2b3faf29452114f9513ab308ab3e4ec1746ea9ae
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-21 11:52:03 +0900

    Add ログイン処理

--- store/user.js
+++ store/user.js
@@ -1,10 +1,18 @@
 import firebase from '~/plugins/firebase'
 
 export const state = () => ({
-  users: []
+  users: {}
 });
 
-export const mutations = {};
+export const mutations = {
+  createUser(state, payload) {
+    state.users = {
+      name: payload.name,
+      email: payload.email,
+      comment: payload.comment,
+    }
+  },
+};
 
 export const actions = {
   googleLogin() {
@@ -12,6 +20,15 @@ export const actions = {
     const googleAuth_provider = new firebase.auth.GoogleAuthProvider();
     // ログインページにリダイレクトしてログインを行う
     firebase.auth().signInWithRedirect(googleAuth_provider);
+  },
+  customLogin() {
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+  },
+  createUser(context, payload){
+    context.commit('createUser', payload)
   }
 };
 
