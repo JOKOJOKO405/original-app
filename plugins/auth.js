8e93af51549958ef6607b339206e0855bbb8ce3a
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-22 14:38:48 +0900

    TODO ログイン判定メモ書き

--- plugins/auth.js
+++ plugins/auth.js
@@ -3,6 +3,17 @@ export default ({ app }, inject) => {
     return new Promise((resolve) => {
       app.$fireAuth.onAuthStateChanged((auth) => {
         resolve(auth || null)
+        if(auth){
+          $nuxt.$store.dispatch('user/setLogin', auth)
+          // TODO ユーザー情報取得
+    //       var displayName = user.displayName;
+    // var email = user.email;
+    // var emailVerified = user.emailVerified;
+    // var photoURL = user.photoURL;
+    // var isAnonymous = user.isAnonymous;
+    // var uid = user.uid;
+    // var providerData = user.providerData;
+        }
       })
     })
   })
