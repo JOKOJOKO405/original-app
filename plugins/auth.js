4a337e271da3b5b063912cb66bbc8038ea55bfe4
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-22 15:42:25 +0900

    Remove　メモ書き削除

--- plugins/auth.js
+++ plugins/auth.js
@@ -5,14 +5,6 @@ export default ({ app }, inject) => {
         resolve(auth || null)
         if(auth){
           $nuxt.$store.dispatch('user/setLogin', auth)
-          // TODO ユーザー情報取得
-    //       var displayName = user.displayName;
-    // var email = user.email;
-    // var emailVerified = user.emailVerified;
-    // var photoURL = user.photoURL;
-    // var isAnonymous = user.isAnonymous;
-    // var uid = user.uid;
-    // var providerData = user.providerData;
         }
       })
     })
