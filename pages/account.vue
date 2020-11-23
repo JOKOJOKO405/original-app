5fe0ac007dc42120c20634cef7c7a12255031657
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-23 11:50:17 +0900

    Add アカウント登録ページ

---
+++ pages/account.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="inner">
+    <EditProfile label="プロフィールを作成しよう" />
+  </div>
+</template>
+
+<script>
+import EditProfile from '~/components/EditProfile'
+export default {
+  middleware: ['checkAccount'],
+  components: {
+    EditProfile,
+  }
+}
+</script>
+
+<style lang="scss">
+  .modalCard__container{
+    padding: 0;
+  }
+</style>
