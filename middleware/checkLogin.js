3d47d108fc3a739ac8bc3d6f73579eaecd0370c9
JOKOJOKO405 <biz.kj405@gmail.com>
2020-12-03 06:04:21 +0900

    Add ログイン画面遷移追記

--- middleware/checkLogin.js
+++ middleware/checkLogin.js
@@ -1,12 +1,13 @@
-export default function ({ redirect, app }) {
+export default function ({ redirect, app, store }) {
   app.$fireAuth.onAuthStateChanged((user) => {
     // const { uid, displayName, email } = user;
     const uid = user.uid
-    // console.log(user.uid);
     if(user){
-      
-      $nuxt.$store.dispatch('user/setLogin', user);
+      store.dispatch('user/setLogin', user);
       redirect(`/user/${uid}`)
+    }else{
+      redirect('login')
+      store.dispatch('user/setLogout')
     }
   })
 }
